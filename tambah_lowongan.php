9ed1ff824eeb3dd1084e64154a9e259b4eb3d481
Dimas Adjie Wibowo <dimasadjiewibowodimas2904@gmail.com>
2025-10-08 14:44:19 +0700

    memperbarui file

--- tambah_lowongan.php
+++ tambah_lowongan.php
@@ -9,6 +9,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
   header('Location: login.php');
   exit;
 }
+#pp
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $nama_perusahaan = $_POST['nama_perusahaan'];
